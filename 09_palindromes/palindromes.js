c40c174047a385e2664faf46b43b5abcb1dba842
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 18:30:26 +0100

    Completed exercise 09, palindromes. Note: This was not verified in the tests, but I know my solution will treat special characters the same as punctuation or spaces. I could revisit this exercise in the future if I want to support accents and such.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = string => {
+    // First step: making sure the method is case insensitive
+    string = string.toLowerCase();
 
-};
+    // Using regex to remove characters which are not alphanumeric
+    string = string.replace(/[^a-z0-9]/gi,"");
+
+    // Last step: reverse and check if it worked!
+    return (string === reverseString(string)) ? true : false;
+}
+
+
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
