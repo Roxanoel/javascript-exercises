81d7dcefdf27b44b976f82fdfb44a540ecf4637c
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 15:32:33 +0100

    Completed exercise 04, removeFromArray. Solution could likely be optimised later, as I am nesting two for loops (likely O(n²))

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
 
+    let newArray = []
+    const argsArray = toRemove;
+
+    for (let i = 0; i < array.length ; i++)
+    {
+        if (!checkIfArgsMatchValue(argsArray, array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
+
+function checkIfArgsMatchValue(array, valueToCheck) {
+    for (let i = 0; i < array.length; i++) {
+        // If there is a match between the argument and the value to check, return true
+        if (array[i] === valueToCheck) return true;
+    }
+    // If no match was found, return false.
+    return false;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
