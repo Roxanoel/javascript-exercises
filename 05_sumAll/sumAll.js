9ac1894714af1c7c4dd304351d484c165bdd7fb9
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 15:52:25 +0100

    Completed exercise 05, sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) { 
+    // Safeguard against invalid values
+    if (!(typeof n1 === "number") 
+        || !(typeof n2 === "number" ) 
+        || n1 < 0
+        || n2 < 0)
+    {return "ERROR"};
+    
+    // This variable will store the result of the addition
+    let sum = 0;
 
+    // Before the loop, determine which number is lower and which is greater.
+    const min = (n1 <= n2) ? n1 : n2;
+    const max = (n1 > n2) ? n1 : n2;
+
+    // Loop starts at min and ends at max inclusively. Adds all results.
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
