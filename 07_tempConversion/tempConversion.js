7f76eb6ab1151274961f82f0ec04e58a2d5c57c3
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 16:29:39 +0100

    Completed exercise 07, tempConversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  // Safeguard against invalid values
+  if (!(typeof f === "number")) {return "ERROR"};
 
+  // Apply formula and round
+  return roundToOneDecimal((f - 32) * (5 / 9));
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  // Safeguard against invalid values
+  if (!(typeof c === "number")) {return "ERROR"};
 
+  // Apply formula and round
+  return roundToOneDecimal(c * (9 / 5) + 32);
 };
+
+function roundToOneDecimal(num) {
+    return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
