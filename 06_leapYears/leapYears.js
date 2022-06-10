2737a52a7a3444dbfaf47b867f87bf8251ae87c3
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 16:11:43 +0100

    Completed exercise 06, leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Safeguard for invalid values
 
+
+    // Checks if conditions are failed
+    if (!(year%4 === 0)) return false;
+
+    if (year%100 === 0 && !(year%400 === 0)) return false;
+
+    // Returns true if no condition was failed
+    return true;
 };
 
 // Do not edit below this line
