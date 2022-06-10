81e6fe47f37337dfaa8f7826981c962cea8e80a4
Roxanoel <roxane.noel92@gmail.com>
2022-06-10 16:49:46 +0100

    Completed exercise 08, calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = array => {
+	// Introduce variable to keep track of sum
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const subtract = function() {
-	
+const multiply = array => {
+
+  let product = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    product *= array[i];
+  }
+  
+  return product;
 };
 
-const sum = function() {
-	
-};
+const power = (num1, num2) => num1 ** num2;
 
-const multiply = function() {
+const factorial = num => {
+  let result = num;
 
-};
+  // Returns 1 when value is 0
+  if (result === 0) return 1;
 
-const power = function() {
-	
-};
+  // For other values, runs this loop
+  for (let i = num - 1; i > 0; i--){
+    result *= i;
+  }
 
-const factorial = function() {
-	
-};
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
