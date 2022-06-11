a43831bde48aeab6cde1af9f308f3f415a89e7b1
Roxanoel <roxane.noel92@gmail.com>
2022-06-11 10:16:00 +0100

    Completed exercise 11, getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    // define new array to return
+    let result = [];
+    
+    bookArray.forEach(element => {
+        result.push(element.title);
+    });
 
+    return result;
 };
 
 // Do not edit below this line
