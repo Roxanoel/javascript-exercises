fa5ba9e9ffd1943606fb1460252588dbefa6f1c8
Roxanoel <roxane.noel92@gmail.com>
2022-06-13 20:13:47 +0100

    Completed exercise 12 (findTheOldest).

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,11 +1,19 @@
 const findTheOldest = array => {
-    let oldestPerson;
-
-    for (let i = 1; i < array.length; i++)
-    {
-        
+    return array.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = calculateAge(oldestPerson);
+        const currentAge = calculateAge(currentPerson);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
     }
-};
+    )
+}
+
+function calculateAge(person) {
+    if (!('yearOfDeath' in person)) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
